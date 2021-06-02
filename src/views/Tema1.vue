7c4f76eaf8080e9d3ada31e26d5aeceaf9f97f7e
john <83730668+DevJohn2021@users.noreply.github.com>
2021-06-01 23:59:07 -0500

    avance

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -9,14 +9,63 @@
     
     p.mb-5 La atención en salud por profesional de medicina, enfermería, medicina familiar o pediatría, que se brinda en el marco de la valoración integral por momento de curso de vida, es un procedimiento que articula una serie de atenciones esenciales para aportar a los resultados en salud, desde una perspectiva de integralidad e incorporando elementos y principios del enfoque de curso de vida. Esta atención tiene como objetivos:
 
-    .h6.text-center Objetivos de la consulta de valoración integral en salud por Curso de Vida
+    .h6.text-center.mb-5 Objetivos de la consulta de valoración integral en salud por Curso de Vida
 
     .row.justify-content-center.mb-4
       .col-lg-6.align-self-center
-        figure
-          img(src='@/assets/curso/tema1/img1.svg', alt='Objetivos de la consulta de valoración integral en salud por Curso de Vida')
+        figure(style="text-align: -webkit-center;")
+          img(src='@/assets/curso/tema1/img1.jpg', alt='Objetivos de la consulta de valoración integral en salud por Curso de Vida', style="width:380px;")
       .col-lg-6.align-self-center
+        .contenedor
+          hr.hr.mx-auto
+          .recuadro_imagen.px-2
+            img(src='@/assets/curso/tema1/num1.svg', alt='Texto que describa la imagen', style="margin-right:2rem !important;").m-auto 
+            p Construir el plan de cuidado primario, el cual deberá ser concertado con el usuario desde la adolescencia. 
+        .contenedor
+          hr.hr.mx-auto
+          .recuadro_imagen.px-2
+            img(src='@/assets/curso/tema1/num2.svg', alt='Texto que describa la imagen', style="margin-right:2rem !important;").m-auto
+            p Identificar tempranamente la exposición o presencia de factores de riesgo con el fin de prevenirlos o derivarlos para su manejo oportuno.
+        
+        .contenedor
+          hr.hr.mx-auto
+          .recuadro_imagen.px-2
+            img(src='@/assets/curso/tema1/num3.svg', alt='Texto que describa la imagen', style="margin-right:2rem !important;").m-auto
+            p Potenciar capacidades, habilidades y prácticas para promover el cuidado de la salud y fortalecer los procesos de desarrollo mediante la información en salud. 
+        
+        .contenedor
+          hr.hr.mx-auto
+          .recuadro_imagen.px-2
+            img(src='@/assets/curso/tema1/num4.svg', alt='Texto que describa la imagen', style="margin-right:2rem !important;").m-auto
+            p Detectar de forma temprana alteraciones que afecten negativamente la salud y el proceso de crecimiento y desarrollo, con el fin de referirlas para su manejo oportuno.
+          
+          .contenedor
+          hr.hr.mx-auto
+          .recuadro_imagen.px-2
+            img(src='@/assets/curso/tema1/num5.svg', alt='Texto que describa la imagen', style="margin-right:2rem !important;").m-auto
+            p Valorar y hacer seguimiento integral a la salud a la persona en todo el curso de la vida; así como al desarrollo integral (físico, cognitivo, social) especialmente desde la primera infancia hasta la juventud.
+          hr.hr.mx-auto
+    
+    #t_1_1.titulo-segundo
+      .h4.mb-4 1.1 Talento humano
 
+    p.mb-4 El talento humano que realiza esta atención debe contar con capacidades técnicas e individuales, que permitan valorar de manera integral, la salud de una persona de acuerdo a su momento de curso de vida, reconocer las características y potencialidades, valorar las ventanas de oportunidades, contar con habilidades de comunicación, reconocer los efectos acumulativos para la salud a lo largo de la vida y la influencia de los sucesos vitales, así como la importancia de las vidas interconectadas en el desarrollo; gestionar adecuadamente los factores de riesgo, conocer e implementar los escenarios de educación para la salud, de acuerdo a las orientaciones emitidas al respecto para identificar y potenciar capacidades, habilidades y prácticas de cuidado de la salud. Así mismo, debe conocer aspectos bioéticos y legales, especialmente en relación con la atención en salud de poblaciones vulnerables, el abordaje de género y en algunos momentos del curso de vida (por ejemplo, adolescencia). Para la primera infancia se define unos factores o condiciones asociadas a mayor riesgo, en las cuales la atención debe ser desarrollada por profesional de pediatría. Para el caso de vejez, se propone que el profesional de medicina o medicina familiar cuente con conocimiento y experiencia en gerontología y geriatría, entre otros aspectos (Ministerio de Salud y Protección Social, 2018).
+
+    p.mb-4 Para los momentos que corresponda, la Resolución 3280 de 2018 define que el profesional de enfermería podrá realizar los procedimientos y ordenar los medicamentos, insumos, dispositivos y otros procedimientos de acuerdo con lo definido en esta intervención/atención. El tamizaje de agudeza visual para los niños en primera infancia, debe ser realizado por personal entrenado: medicina general, especialista u optómetra.
+    
+    h6.mb-4  Atenciones incluidas en el procedimiento
+    p.mb-5 El procedimiento tiene una serie de atenciones que apuntan a los objetivos señalados, como se puede observar en la siguiente infografía.
+
+    .row.justify-content-center.mb-5
+      .col-lg-10.align-self-center
+        figure(style="text-align: -webkit-center;")
+          img(src='@/assets/curso/tema1/img2.svg', alt='Atenciones incluidas en el procedimiento', style="width:450px;")
+    
+    hr
+
+    #t_1_2.titulo-segundo
+      .h4 1.2 Descripción general del procedimiento
+        
 
 
 
